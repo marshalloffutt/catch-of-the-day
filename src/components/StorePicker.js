a36c5fdff0e0f17038be4a8280dac5e6450c920d
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-24 11:14:37 -0700

    adds createRef to storepicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  myInput = React.createRef();
+
+  goToStore(e) {
+    // 1. Stop the form from submitting
+    e.preventDefault();
+    // 2. Get the text from input
+    console.log(this);
+    // 3. Change the page to /store/whatever-they-entered
+    
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
